5e22248f768c03660d3d514d3daf08ce42c65925
Joshua Lochner <admin@xenova.com>
2020-07-26 02:29:35 +0200

    added support for model directory

--- src/encode.py
+++ src/encode.py
@@ -5,13 +5,15 @@
 
 import argparse
 import numpy as np
+import os
 
 import encoder
 from load_dataset import load_dataset
 
 parser = argparse.ArgumentParser(
-    description='Pre-encode text files into tokenized training set.',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	description='Pre-encode text files into tokenized training set.',
+	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--models_dir', metavar='MODELS_DIR', type=str, default='models', help='Path to parent folder containing model subfolders')
 parser.add_argument('--model_name', metavar='MODEL', type=str, default='124M', help='Pretrained model name')
 parser.add_argument('--combine', metavar='CHARS', type=int, default=50000, help='Concatenate files with <|endoftext|> separator into chunks of this minimum size')
 parser.add_argument('--encoding', type=str, default='utf-8', help='Set the encoding for reading and writing files.')
@@ -19,13 +21,14 @@ parser.add_argument('in_text', metavar='PATH', type=str, help='Input file, direc
 parser.add_argument('out_npz', metavar='OUT.npz', type=str, help='Output file path')
 
 def main():
-    args = parser.parse_args()
-    enc = encoder.get_encoder(args.model_name)
-    print('Reading files')
-    chunks = load_dataset(enc, args.in_text, args.combine, encoding=args.encoding)
-    print('Writing', args.out_npz)
-    np.savez_compressed(args.out_npz, *chunks)
+	args = parser.parse_args()
+	models_dir = os.path.expanduser(os.path.expandvars(args.models_dir))
+	enc = encoder.get_encoder(args.model_name,models_dir)
+	print('Reading files')
+	chunks = load_dataset(enc, args.in_text, args.combine, encoding=args.encoding)
+	print('Writing', args.out_npz)
+	np.savez_compressed(args.out_npz, *chunks)
 
 
 if __name__ == '__main__':
-    main()
+	main()
